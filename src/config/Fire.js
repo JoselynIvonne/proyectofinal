73c8949d563a070f2e578c5955ba9e2512e2e09a
JoselynIvonne <joselyn.maffare@gmail.com>
2019-07-31 02:18:39 -0500

    Codificacion del login

---
+++ src/config/Fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+const config = {/* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+    apiKey: "AIzaSyCRA2PV5Q1mw_M8jn_5FbUhSEc7AYSQHwo",
+    authDomain: "gyanimacion.firebaseapp.com",
+    databaseURL: "https://gyanimacion.firebaseio.com",
+    projectId: "gyanimacion",
+    storageBucket: "gyanimacion.appspot.com",
+    messagingSenderId: "698067989319",
+    appId: "1:698067989319:web:12ff8c512fc3ce6a"
+}
+const fire =firebase.initializeApp(config);
+export default fire;
